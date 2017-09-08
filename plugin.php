18d2bf81c953bc62044d724fc4c8061c3d1091f1
Electric Paper Evaluationssysteme GmbH <tal@evasys.de>
2017-09-08 16:27:37 +0200

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -31,12 +31,12 @@ E-Mail: info@evasys.de
 $id = "evasys";
 
 // code version; must be changed for all code changes
-$version = "1.0.3";
+$version = "1.0.4";
 
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "4.2.0";
-$ilias_max_version = "5.0.4";
+$ilias_max_version = "5.2.7";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Electric Paper Evaluationssysteme GmbH";
